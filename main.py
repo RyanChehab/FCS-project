5a454c577de26436355f908dfb4856e9b384c003
comradeRyan <ryanchehab20@gmail.com>
2024-07-16 20:11:35 +0300

    Add files via upload
    building the welcome function

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("HEllo TEst")
+def Welcome():
+  print("Hello!")
+  while True:
+    try:
+      n=int(input("""
+1. To go to the driver's menu
+2. To go to the cities menu
+3. To exit the system
+Please enter : """))
+      if n==1:
+        print("function1")
+      elif n==2:
+        print("function2")
+      elif n==3:
+        quit()
+    except ValueError:
+      print("Invalid input! please try again. ")
+
+Welcome()
